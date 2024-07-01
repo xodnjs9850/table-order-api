10ea5404d3b5c6157c44f3a82eeb22b198058485
ktw <xodnjs9850@gmail.com>
2024-07-01 13:59:36 +0900

    first commit

---
+++ src/main/java/com/tableOrder/table_order/TableOrderApplication.java
@@ -0,0 +1,13 @@
+package com.tableOrder.table_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tableOrder/table_order/TableOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tableOrder.table_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
